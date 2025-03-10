eb53f2f7739b3ffd8615fe211740d4b7221dc303
hnyoojin <0dbwls21@gmail.com>
2025-03-10 15:18:34 +0900

    Docs: .env.loval 파일 생성 firebase config 내용을 .env.local 파일로 이동했습니다. .env.local 파일은 .gitignore에 넣어둬서 깃헙엔 안올라가요

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,13 @@ import "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDNR9uNwH8b75HyuTFHRzpUE5AH1sN0_pc",
-  authDomain: "tutto-day-tracker.firebaseapp.com",
-  projectId: "tutto-day-tracker",
-  storageBucket: "tutto-day-tracker.firebasestorage.app",
-  messagingSenderId: "234307792246",
-  appId: "1:234307792246:web:09fc633228cbc9ebaf1e57",
-  measurementId: "G-BWFFVBX89S",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
